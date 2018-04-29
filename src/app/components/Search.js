000c85990bb56f1356756e87adab99fd373599c0
Lee-Martin Clarke <mobeel5356@gmail.com>
2018-04-29 03:56:00 -0400

    secondary search now rorks

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -15,13 +15,13 @@ export class Search extends React.Component{
         const { word, found } = this.state;
         fetch(`http://localhost:1337/find?keyword=${this.state.toSearch}`)
             .then(res => res.json())
-            .then(results => this.setState({found}, () => console.log("fetched", results)));
+            .then(results => this.setState({found:results}, () => console.log("fetched", found)));
     }
     render(){
         return(
             <Main>
                 <div style={{width: '100%'}}>
-                    <div className="align text--center" style={{width: '100%', top: '80px'}}>
+                    <div className="align text--center" style={{width: '100%', top: '110px'}}>
 
                             <div className="form__field center-x" style={{whiteSpace: 'nowrap'}}>
                                 <input className="search form__input" id="search" type="text" style={{color:'white',marginRight: '8px'}} name="search" placeholder="What book are you looking for?" required
@@ -36,12 +36,55 @@ export class Search extends React.Component{
                     </div>
                 </div>
                 <div style={{textAlign: 'center', marginTop: '110px'}}>
-                    <h1>Results</h1>
-                    <ul>
+
                         {this.state.found.map(books =>
-                        <li key={books.id}>{ books.title }</li>
-                        )}
-                    </ul>
+
+
+                        <div>
+
+
+                            <table className="center-x" style={{width:800, color:'white', borderColor:'#2c3338', position: 'relative'}}>
+                                <tr>
+                                    <th> </th>
+                                </tr>
+
+                            <tr>
+                                <td style={{width:150, height:170}} rowSpan="4">
+                                    <img src={books.picLoc} style={{width: 150, height: 170 }}/>
+                                </td>
+                                <td>
+                                    Title: {books.title}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td> Author: {books.author}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td> Description: {books.description}
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td style={{textAlign:'center'}} height="70px">
+
+                                <input className="submittt" style={{display:'inline'}} type="button" name={books.bookLoc} value="DOWNLOAD" />
+
+                                    <p style={{display:'none'}}>Must be signed in to download</p>
+                                </td>
+
+                            </tr>
+                </table>
+                            <br/>
+                            <br/>
+                            <hr style={{width:'79%', borderColor:'rgb(37, 42, 45)'}}/>
+                            <br/>
+                            <br/>
+                        </div>
+
+                            )}
+
+
                 </div>
             </Main>
         );

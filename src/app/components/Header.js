e494ebbef342c599e5f2d650012ebfed5e003efe
Lee-Martin Clarke <mobeel5356@gmail.com>
2018-04-29 17:18:23 -0400

    logout function added

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -6,14 +6,60 @@ import {browserHistory,withRouter, Link} from "react-router-dom";
 import { bake_cookie, read_cookie, delete_cookie } from 'sfcookies';
 
 
-
 export class Header extends React.Component {
-    onNavSignIn(){
-        this.props.history.push("/login");
+
+    constructor(){
+        super();
+        this.cookie_key1 = 'eLibUser';
+        this.cookie_key2 = 'eLibName';
+        this.cookie_key3 = 'eLibLvl';
+
     }
+    loggedIn(){
+        const user = read_cookie(this.cookie_key1);
+        const name = read_cookie(this.cookie_key2);
+        const lvl = read_cookie(this.cookie_key3);
+        if(user==''){
+            return <Link className="submittt" to={"/login"} style={{border: 'none', boxShadow: 'none'}}>
+                Sign In
+            </Link>;
+        }else{
+            return <a className="submittt" style={{border: 'none', boxShadow: 'none'}} onClick={() => this.logOut()}>
+                Sign Out
+            </a>;
+        }
+        }
+
+        logOut(){
+            delete_cookie(this.cookie_key1);
+            delete_cookie(this.cookie_key2);
+            delete_cookie(this.cookie_key3);
+            document.location.replace('/login');
+        }
+
+        showName(){
+            const user = read_cookie(this.cookie_key1);
+            const name = read_cookie(this.cookie_key2);
+            const lvl = read_cookie(this.cookie_key3);
+            if(user==''){
+                return <div className="text--center center-x" style={{top:'0px', fontSize:'2em', paddingTop:'30px', position:'absolute'}}>
+                    Hello, Guest
+                </div>
+            }else{
+                return <div className="text--center center-x" style={{top:'0px', fontSize:'2em', paddingTop:'30px', position:'absolute'}}>
+                    Hello, {name}
+                </div>
+            }
+        }
+
+
     render(){
-        const cookie_key2 = 'eLibName';
-        const name = read_cookie(cookie_key2);
+
+        const user = read_cookie(this.cookie_key1);
+        const name = read_cookie(this.cookie_key2);
+        const lvl = read_cookie(this.cookie_key3);
+
+
         return(
             <div>
             <div className="bar">
@@ -23,13 +69,11 @@ export class Header extends React.Component {
 
                 </div>
                 <div className="fl-rt " style={{padding:'10px', top:0}}>
-                    <Link className="submittt" to={"/login"} style={{border: 'none', boxShadow: 'none' }}>
-                        Sign In
-                    </Link>
+
+                    {this.loggedIn()}
+
                 </div>
-                <div className="text--center center-x" style={{top:'0px', fontSize:'2em', paddingTop:'30px', position:'absolute'}}>
-                    Hey, {name}
-                </div>
+                {this.showName()}
 
             </div>
                 <div>
